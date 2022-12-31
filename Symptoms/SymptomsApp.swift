ebfff4a3a4f7ea61cc116bfbc968720eba6d5559
Dermot O'Sullivan <dermot.osullivan2@ing.com>
2022-12-31 15:57:26 +1100

    Initial Commit

---
+++ Symptoms/SymptomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymptomsApp.swift
+//  Symptoms
+//
+//  Created by Dermot O'Sullivan on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SymptomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

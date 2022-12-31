a29ff148d959cdee60a1c39592727d9d56fcbdbd
Dermot O'Sullivan <dermot.osullivan2@ing.com>
2022-12-31 16:10:31 +1100

    Add watch target

---
+++ SymptomsWatch Watch App/SymptomsWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymptomsWatchApp.swift
+//  SymptomsWatch Watch App
+//
+//  Created by Dermot O'Sullivan on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SymptomsWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4f7541fc1adcb207155cba699a41dd7413163499
Dermot O'Sullivan <dermot.osullivan2@ing.com>
2023-01-02 13:10:14 +1100

    Setup packages

--- Symptoms/ContentView.swift
+++ Symptoms/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+            Button {
+                HealthS
+            } label: {
+                <#code#>
+            }
+
         }
         .padding()
     }

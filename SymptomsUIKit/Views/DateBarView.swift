19dc9e08666f324bd42275bfdf703723f67634fa
Dermot O'Sullivan <dermot.osullivan2@ing.com>
2023-01-13 14:24:28 +1100

    Improve date picker

--- SymptomsUIKit/Views/DateBarView.swift
+++ SymptomsUIKit/Views/DateBarView.swift
@@ -25,42 +25,14 @@ import UIKit
 
 final class DateBarView: UIView {
     
-    private(set) var sampleEntryDate = Date()
-    private(set) var previousEntryDate: Date?
-    
-    func updateSampleEntryLabel(to newDate: Date) {
-        
+    var sampleEntryDate: Date {
+        sampleEntryDatePicker.date
     }
-    
-    func updatePreviousEntryLabel(to newDate: Date?) {
-        
-
-    }
-    
-    private var sampleEntryLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        label.backgroundColor = .clear
-        label.textColor = .label
-        label.textAlignment = .center
-        label.lineBreakMode = .byTruncatingMiddle
-        return label
-    }()
     
     private var sampleEntryDatePicker: UIDatePicker = {
         let picker = UIDatePicker()
         picker.preferredDatePickerStyle = .compact
         return picker
-    }()
-    
-    private var previousEntryLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .caption1)
-        label.backgroundColor = .clear
-        label.textColor = .secondaryLabel
-        label.textAlignment = .center
-        label.lineBreakMode = .byTruncatingMiddle
-        return label
     }()
     
     init() {
@@ -75,18 +47,11 @@ final class DateBarView: UIView {
     private func setupView() {
         backgroundColor = .systemPink
         addSubview(sampleEntryDatePicker)
-        addSubview(previousEntryLabel)
         sampleEntryDatePicker.translatesAutoresizingMaskIntoConstraints = false
-        previousEntryLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             sampleEntryDatePicker.topAnchor.constraint(equalTo: topAnchor, constant: Constants.spacing),
             sampleEntryDatePicker.centerXAnchor.constraint(equalTo: centerXAnchor),
-            sampleEntryDatePicker.widthAnchor.constraint(equalTo: widthAnchor, constant: -Constants.spacing * 2),
-            
-            sampleEntryDatePicker.bottomAnchor.constraint(equalTo: previousEntryLabel.topAnchor, constant: Constants.spacing),
-            previousEntryLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.spacing),
-            previousEntryLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Constants.spacing),
-            previousEntryLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Constants.spacing)
+            sampleEntryDatePicker.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Constants.spacing)
         ])
     }
     
